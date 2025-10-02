44ced7eefa61d251e8fa663b7232eaad3387d9aa
Re1otsu <lbs2005v@gmail.com>
2025-10-02 23:00:32 +0500

    2 лаб

--- Data science/1lr.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
-df = df.drop(columns =
-['anaemia','diabetes','high_blood_pressure','sex','smoking','time','DEATH_EVENT'])
-print(df)
